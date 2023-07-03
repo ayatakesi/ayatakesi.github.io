8e7d63e493d0ae5a1e66cfe2bb6f1d28a4b72a64
ayatakesi <ayanokoji.takesi@gmail.com>
2023-07-03 21:11:58 +0000

    Update src/*

---
+++ src/emacs/master/HTML/rebuild.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# rebuild.sh: rebuild hypertext with the previous context.
+#
+# Usage:
+#	% sh rebuild.sh
+#
+cd /home/runner/work/termux-emacs/termux-emacs/emacs && GTAGSCONF=':skip=HTML/,HTML.pub/,tags,TAGS,ID,y.tab.c,y.tab.h,gtags.files,cscope.files,cscope.out,cscope.po.out,cscope.in.out,SCCS/,RCS/,CVS/,CVSROOT/,{arch}/,autom4te.cache/,*.orig,*.rej,*.bak,*~,#*#,*.swp,*.tmp,*_flymake.*,*_flymake,*.o,*.a,*.so,*.lo,*.zip,*.gz,*.bz2,*.xz,*.lzh,*.Z,*.tgz,*.min.js,*min.css:langmap=c\:.c.h,yacc\:.y,asm\:.s.S,java\:.java,cpp\:.c++.cc.hh.cpp.cxx.hxx.hpp.C.H,php\:.php.php3.phtml:' htags -C emacs --gtags --line-number=6 ../ayatakesi.github.io/src/emacs/master
